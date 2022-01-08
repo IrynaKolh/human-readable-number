92ac4eb237a57d646c33d3198dcb0f3ec7f52f34
IrynaKolh <iryna.kolhanova@gmail.com>
2022-01-07 23:43:46 -0500

    Human Readable Number implemented

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    var converter = require('number-to-words');
+
+    return converter.toWords(number).replace('-', ' ');
+
+};
